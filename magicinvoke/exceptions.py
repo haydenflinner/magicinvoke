282d188d295150004ea97b85b8890e09ace59a43
Hayden Flinner <haydenflinner@gmail.com>
2020-05-23 21:13:03 -0400

    Refactor @skippable to be extensible/customizable

--- magicinvoke/exceptions.py
+++ magicinvoke/exceptions.py
@@ -1,5 +1,7 @@
 
 class SaveReturnvalueError(Exception):
+    """Failed to save the return-value of a disk-cached function."""
+    # Returnvalue to avoid mistaking for a ..ValueError
     pass
 
 class DerivingArgsError(Exception):
